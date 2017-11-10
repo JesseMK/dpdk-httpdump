88c7ac997203c103a7eb585c07da8a78af787717
JesseMK <jessewangzh@hotmail.com>
2017-11-10 17:34:51 +0800

    new dir structure

--- include/hd_pcap.h
+++
@@ -1,6 +0,0 @@
-#ifndef _HTTPDUMP_PCAP_
-#define _HTTPDUMP_PCAP_
-
-void httpdump_pcap(const struct pcap_pkthdr *pkthdr, const unsigned char *bytes);
-
-#endif //_HTTPDUMP_PCAP_
--- include/hd_print.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DPDK_HTTPDUMP_PRINT_
-#define _DPDK_HTTPDUMP_PRINT_
-
-#include "hd_pkt.h"
-
-void __print_ts(FILE *output, struct timeval ts);
-void __print_ip(FILE *output, host_t *host);
-
-#endif //_DPDK_HTTPDUMP_PRINT_

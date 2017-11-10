20c95b1e50247265b1f08a6030fe08b631072900
JesseMK <jessewangzh@hotmail.com>
2017-11-10 15:38:42 +0800

    CRLF -> LF

--- hd_init.c
+++ hd_init.c
@@ -2,7 +2,7 @@
  * @Author: JesseMK
  * @Date: 2017-10-19 19:57:53
  * @Last Modified by: JesseMK
- * @Last Modified time: 2017-10-19 21:01:02
+ * @Last Modified time: 2017-11-10 15:37:11
  */
 
 #include <rte_eal.h>
--- hd_init.h
+++ hd_init.h
@@ -2,7 +2,7 @@
  * @Author: JesseMK
  * @Date: 2017-10-19 19:57:41
  * @Last Modified by: JesseMK
- * @Last Modified time: 2017-10-19 20:46:25
+ * @Last Modified time: 2017-11-10 15:37:11
  */
 
 #ifndef _DPDK_HTTPDUMP_INIT_

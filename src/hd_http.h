ef03a835da276aa3e9b3ed99545358814e61015a
JesseMK <jessewangzh@hotmail.com>
2017-11-10 17:35:01 +0800

    new dir structure

---
+++ src/hd_http.h
@@ -0,0 +1,13 @@
+#ifndef _HTTPDUMP_HTTP_
+#define _HTTPDUMP_HTTP_
+
+#include "hd_pkt.h"
+
+#define HTTP_HEADER_MINLEN 12
+#define HTTP_FIELD_MINLEN 5
+#define HTTP_REQUEST_LINE_MINLEN 14
+#define HTTP_STATUS_LINE_MINLEN 12
+
+void httpdump_http(unsigned char *data, uint32_t len, struct timeval ts, host_t *src, host_t *dst);
+
+#endif //_HTTPDUMP_HTTP_
---
+++ src/hd_init.h
@@ -0,0 +1,13 @@
+/*
+ * @Author: JesseMK
+ * @Date: 2017-10-19 19:57:41
+ * @Last Modified by: JesseMK
+ * @Last Modified time: 2017-11-10 15:37:11
+ */
+
+#ifndef _DPDK_HTTPDUMP_INIT_
+#define _DPDK_HTTPDUMP_INIT_
+
+void init_dpdk_eal();
+
+#endif /* ifndef _DPDK_HTTPDUMP_INIT_ */
